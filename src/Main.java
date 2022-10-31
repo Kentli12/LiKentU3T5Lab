a7e442ad1c7e57e1ae7b6b5c1dc7fdb88860759b
student <sample@bths.edu>
2022-10-31 12:32:47 -0400

    Work

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int p = 5;
+        int q = 0;
+
+        if (p < 3 || (p / q) == 2)
+        {
+            System.out.println("it’s true :)");
+        }
+        else
+        {
+            System.out.println("it’s false :(");
+        }
+    }
+}
